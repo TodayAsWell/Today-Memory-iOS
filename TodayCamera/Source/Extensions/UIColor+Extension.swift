4102d56618e7d2df9c10aa19f0ad66a141ec2af1
junha <goodjunha@gmail.com>
2023-06-01 18:40:31 +0900

    🫧 :: color 추가

---
+++ TodayCamera/Source/Extensions/UIColor+Extension.swift
@@ -0,0 +1,21 @@
+//
+//  UIColor+Extension.swift
+//  TodayCamera
+//
+//  Created by 박준하 on 2023/06/01.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    //Point
+    static let Yellow: UIColor = UIColor(named: "Yellow")!
+    
+    //Primary
+    static let Gray1: UIColor = UIColor(named: "Gray1")!
+    static let Gray2: UIColor = UIColor(named: "Gray2")!
+    static let Gray3: UIColor = UIColor(named: "Gray3")!
+    static let Gray4: UIColor = UIColor(named: "Gray4")!
+    static let White: UIColor = UIColor(named: "White")!
+}
