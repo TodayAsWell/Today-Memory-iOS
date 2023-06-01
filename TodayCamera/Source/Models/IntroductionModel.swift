5da26570d0a7ef14ddff4c46e7dca3e6ef38306c
junha <goodjunha@gmail.com>
2023-06-01 18:39:34 +0900

    🫧 :: 소개 모델 설정

---
+++ TodayCamera/Source/Models/IntroductionModel.swift
@@ -0,0 +1,20 @@
+//
+//  IntroductionModel.swift
+//  TodayCamera
+//
+//  Created by 박준하 on 2023/06/01.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+struct IntroductionModel {
+    var title: String
+    var image: UIImage
+    
+    init(title: String, image: UIImage) {
+        self.title = title
+        self.image = image
+    }
+}
